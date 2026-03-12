b8d60e6310af22d73c84cf6c5404037175dd73d5
udayasherkar94-prog <udayasherkar94@gmail.com>
2026-03-12 20:21:26 +0530

    Add JavaScript for calculator logic and button operations

---
+++ Calculator/script.js
@@ -0,0 +1,40 @@
+let display = document.getElementById("display");
+
+function append(value){
+    display.value += value;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+/* Keyboard Support */
+
+document.addEventListener("keydown",function(e){
+
+    if((e.key >= 0 && e.key <= 9) || "+-*/.".includes(e.key)){
+        append(e.key);
+    }
+
+    else if(e.key === "Enter"){
+        calculate();
+    }
+
+    else if(e.key === "Backspace"){
+        display.value = display.value.slice(0,-1);
+    }
+
+    else if(e.key === "Escape"){
+        clearDisplay();
+    }
+
+});
